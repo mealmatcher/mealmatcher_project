a13063c40a17db8e01cd845fc96324cd39d8a079
Drew Wallace <drewwa1ace@gmail.com>
2015-04-05 16:53:14 -0400

    Fixed date returning "value/value". Made the homepage slightly fancier. Changed how time dropdown works.

--- mealmatcher_app/forms.py
+++ mealmatcher_app/forms.py
@@ -8,8 +8,8 @@ from django.db import models
 class MealForm(forms.ModelForm):
   # filled in by JS, mm/dd but not necessarily leading 0s
   date_mdy = forms.CharField(
-    max_length = 5,
     widget = forms.Select(attrs={'id': 'dateDropdown'}),
+    max_length = 5,
     help_text = "on"
     # choices = ( # HACK(drew): this tuple must include all options filled in dynamically in findmeal.html
     #   ("4/1", "4/1"),
@@ -37,26 +37,27 @@ class MealForm(forms.ModelForm):
   )
 
   # filled in by JS, hour:minutes-hour:minutes
-  date_time = forms.ChoiceField(
+  date_time = forms.CharField(
     widget = forms.Select(attrs={'id': 'timeDropdown'}),
+    max_length = 11,    
     help_text = "at",
-    choices = ( # HACK(drew): this tuple must include all options filled in dynamically in findmeal.html
-      ("7:30-8:00", "7:30 - 8:00"),
-      ("8:00-8:30", "8:00 - 8:30"),
-      ("8:30-9:00", "8:30 - 9:00"),
-      ("9:00-9:30", "9:00 - 9:30"),
-      ("9:30-10:00", "9:30 - 10:00"),
-      ("10:00-10:30", "10:00 - 10:30"),
-      ("10:30-11:00", "10:30 - 11:00"),
-      ("11:00-11:45", "11:00 - 11:45"),
-      ("11:45-12:30", "11:45 - 12:30"),
-      ("12:30-1:15", "12:30 - 1:15"),
-      ("1:15-2:00", "1:15 - 2:00"),
-      ("5:00-5:45", "5:00 - 5:45"),
-      ("5:45-6:30", "5:45 - 6:30"),
-      ("6:30-7:15", "6:30 - 7:15"),
-      ("7:15-8:00", "7:15 - 8:00"),
-    )
+    # choices = ( # HACK(drew): this tuple must include all options filled in dynamically in findmeal.html
+    #   ("7:30-8:00", "7:30 - 8:00"),
+    #   ("8:00-8:30", "8:00 - 8:30"),
+    #   ("8:30-9:00", "8:30 - 9:00"),
+    #   ("9:00-9:30", "9:00 - 9:30"),
+    #   ("9:30-10:00", "9:30 - 10:00"),
+    #   ("10:00-10:30", "10:00 - 10:30"),
+    #   ("10:30-11:00", "10:30 - 11:00"),
+    #   ("11:00-11:45", "11:00 - 11:45"),
+    #   ("11:45-12:30", "11:45 - 12:30"),
+    #   ("12:30-1:15", "12:30 - 1:15"),
+    #   ("1:15-2:00", "1:15 - 2:00"),
+    #   ("5:00-5:45", "5:00 - 5:45"),
+    #   ("5:45-6:30", "5:45 - 6:30"),
+    #   ("6:30-7:15", "6:30 - 7:15"),
+    #   ("7:15-8:00", "7:15 - 8:00"),
+    # )
   ) 
   
   meal_time = forms.ChoiceField(
