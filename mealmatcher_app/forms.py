a30a95e517edcc2ea2b3e0fc7f08929046f23223
Drew Wallace <drewwa1ace@gmail.com>
2015-04-05 16:10:00 -0400

    Fixed find meal page to use django template forms.

--- mealmatcher_app/forms.py
+++ mealmatcher_app/forms.py
@@ -6,44 +6,49 @@ import datetime
 from django.db import models
 
 class MealForm(forms.ModelForm):
-	date_mdy = forms.CharField(max_length=5)   #mm/dd but not necessarily leading 0s
-	date_time = forms.CharField(max_length=11) #hour:minutes-hour:minutes
-	MEALS = (
-		('B', 'Breakfast'),
-		('L', 'Lunch'),
-		('D', 'Dinner'),
-	)
-	meal_time = forms.CharField(max_length=1)#, choices=MEALS)
-	LOCATIONS = (
-		('WH', 'Whitman'),
-		('RM', 'Rocky/Mathey'),
-		('BW', 'Butler/Wilson'),
-		('F', 'Forbes'),
-	)
-	location = forms.CharField(max_length=2)#, choices=LOCATIONS)
+  # filled in by JS, mm/dd but not necessarily leading 0s
+  date_mdy = forms.ChoiceField(
+    widget = forms.Select(attrs={'id': 'dateDropdown'}),
+    help_text = "on",
+    choices = ()
+  ) 
 
-	attire1 = models.CharField(max_length=100)
+  # filled in by JS, hour:minutes-hour:minutes
+  date_time = forms.ChoiceField(
+    widget = forms.Select(attrs={'id': 'timeDropdown'}),
+    help_text = "at",
+    choices = ()
+  ) 
+  
+  meal_time = forms.ChoiceField(
+    widget = forms.Select(attrs={'id': 'mealDropdown', 'name': 'mealDropdown'}),
+    help_text = "I want to get ",
+    choices = (
+      ('B', 'Breakfast'),
+      ('L', 'Lunch'),
+      ('D', 'Dinner'),
+    )
+  )
 
-	class Meta:
-		model = Meal
-		fields = ('meal_time', 'location', 'attire1',)
-		exclude = ('date', 'attire2', 'users',)
+  location = forms.ChoiceField(
+    help_text = "at ",
+    choices = (
+      ('WH', 'Whitman'),
+      ('RM', 'Rocky/Mathey'),
+      ('BW', 'Butler/Wilson'),
+      ('F', 'Forbes'),
+    )
+  )
+
+  attire1 = models.CharField(
+     help_text = "and I'll be wearing...",
+     max_length=100,  
+   )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  class Meta:
+    model = Meal
+    fields = ('meal_time', 'location', 'date_mdy', 'date_time', 'attire1',)
+    exclude = ('date', 'attire2', 'users',)
 
 
