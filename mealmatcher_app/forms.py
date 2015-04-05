10679a29015fc005bf2e179bc48337cd7090ee91
Drew Wallace <drewwa1ace@gmail.com>
2015-04-05 16:43:03 -0400

    Fixed a syntax error in last commit. Trying a dfferent meal date hack.

--- mealmatcher_app/forms.py
+++ mealmatcher_app/forms.py
@@ -7,33 +7,34 @@ from django.db import models
 
 class MealForm(forms.ModelForm):
   # filled in by JS, mm/dd but not necessarily leading 0s
-  date_mdy = forms.ChoiceField(
+  date_mdy = forms.CharField(
+    max_length = 5,
     widget = forms.Select(attrs={'id': 'dateDropdown'}),
-    help_text = "on",
-    choices = ( # HACK(drew): this tuple must include all options filled in dynamically in findmeal.html
-      ("4/1", "4/1"),
-      ("4/2", "4/2"),
-      ("4/3", "4/3"),
-      ("4/4", "4/4"),
-      ("4/5", "4/5"),
-      ("4/6", "4/6"),
-      ("4/7", "4/7"),
-      ("4/8", "4/8"),
-      ("4/9", "4/9"),
-      ("4/10", "4/10"),
-      ("4/11", "4/11"),
-      ("4/12", "4/12"),
-      ("4/13", "4/13"),
-      ("4/14", "4/14"),
-      ("4/15", "4/15"),
-      ("4/16", "4/16"),
-      ("4/17", "4/17"),
-      ("4/18", "4/18"),
-      ("4/19", "4/19"),
-      ("4/20", "4/20"),
-      ("4/21", "4/21"),
-    )
-  ) 
+    help_text = "on"
+    # choices = ( # HACK(drew): this tuple must include all options filled in dynamically in findmeal.html
+    #   ("4/1", "4/1"),
+    #   ("4/2", "4/2"),
+    #   ("4/3", "4/3"),
+    #   ("4/4", "4/4"),
+    #   ("4/5", "4/5"),
+    #   ("4/6", "4/6"),
+    #   ("4/7", "4/7"),
+    #   ("4/8", "4/8"),
+    #   ("4/9", "4/9"),
+    #   ("4/10", "4/10"),
+    #   ("4/11", "4/11"),
+    #   ("4/12", "4/12"),
+    #   ("4/13", "4/13"),
+    #   ("4/14", "4/14"),
+    #   ("4/15", "4/15"),
+    #   ("4/16", "4/16"),
+    #   ("4/17", "4/17"),
+    #   ("4/18", "4/18"),
+    #   ("4/19", "4/19"),
+    #   ("4/20", "4/20"),
+    #   ("4/21", "4/21"),
+    # )
+  )
 
   # filled in by JS, hour:minutes-hour:minutes
   date_time = forms.ChoiceField(
@@ -55,6 +56,7 @@ class MealForm(forms.ModelForm):
       ("5:45-6:30", "5:45 - 6:30"),
       ("6:30-7:15", "6:30 - 7:15"),
       ("7:15-8:00", "7:15 - 8:00"),
+    )
   ) 
   
   meal_time = forms.ChoiceField(
